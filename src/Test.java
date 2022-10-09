be98882bbe34aff62516d0f01fa1dfd37afc4c1e
Mohammed <mdali@qou.edu>
2022-10-09 09:40:51 +0300

    Adding Test2 class

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-       int a[] = {1,2,3,4,5};
+       int a[] = {1,2,3,4,5,6};
     for (int i:a){
         System.out.println(i);
         }
